2531818671d7533f30d64e7679b890a5be2c0e11
VadymChink <vadim-vsnnschuk@ro.ru>
2022-06-06 11:21:56 +0300

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-project';
+
+  theme: null | string = 'theme-dark' ;
+
+  constructor(private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  toggleTheme() {
+    this.activatedRoute.params.subscribe(() => {
+      if (localStorage.getItem('theme') === 'theme-dark') {
+        localStorage.setItem('theme', 'theme-light');
+      } else {
+        localStorage.setItem('theme', 'theme-dark');
+      }
+      this.theme = localStorage.getItem('theme')
+    })
+  }
+
+
 }
